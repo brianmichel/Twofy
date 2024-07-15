93ccc9c3983ebf4b07ac095126a209bde4972852
Brian Michel <brian.michel@gmail.com>
2024-07-14 20:03:04 -0400

    Update styling

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -1,8 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    }
   },
   plugins: [],
 }
