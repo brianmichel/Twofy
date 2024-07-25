78eb716402c1d17e7b7addee9b6dc396aad2adf9
Brian Michel <brian.michel@gmail.com>
2024-07-24 22:55:11 -0400

    Fix manifest format

--- Frameworks/Features/Sources/ManifestInstallerService/Models/Manifest.swift
+++ Frameworks/Features/Sources/ManifestInstallerService/Models/Manifest.swift
@@ -1,16 +1,28 @@
 import Foundation
 
 public enum ExtensionID: String, Codable {
-    case chromiumProduction = "chrome-extension://gfkpgomgmghnffbledjbhcfcllpdgjog"
-    case chromiumDebug = "chrome-extension://dmlknddeegiihclfnlcgpnekbgahbime"
+    // N.B. The trailing `/` must be present otherwise these may fail to decode
+    // correctly when trying to load the native message host.
+    case chromiumProduction = "chrome-extension://gfkpgomgmghnffbledjbhcfcllpdgjog/"
+    case chromiumDebug = "chrome-extension://dmlknddeegiihclfnlcgpnekbgahbime/"
 }
 
+// Manifest format can be found with more detail
+// online here https://developer.chrome.com/docs/extensions/develop/concepts/native-messaging#native-messaging-host
 public struct Manifest: Codable {
     public let name: String
     public let description: String
     public let path: String
     public let type: String
     public let allowedOrigins: [ExtensionID]
+
+    enum CodingKeys: String, CodingKey {
+        case name
+        case description
+        case path
+        case type
+        case allowedOrigins = "allowed_origins"
+    }
 }
 
 extension Manifest {
